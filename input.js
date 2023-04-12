23f4e4bae6500411495151f9953c40793b78a2ff
hoomili <hasadian@hotmail.com>
2023-04-12 15:32:48 -0700

    finished sending keyboard inputs to server

--- input.js
+++ input.js
@@ -1,5 +1,7 @@
 
-const setupInput = () => {
+let connection;
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -10,6 +12,18 @@ const setupInput = () => {
 const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
   }
 };
 module.exports = { setupInput };
