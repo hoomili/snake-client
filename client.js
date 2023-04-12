b2bf4edd3202f61d3374c20bf559083e4b027072
hoomili <hasadian@hotmail.com>
2023-04-12 16:08:39 -0700

    added the constant file to keep the constants

--- client.js
+++ client.js
@@ -1,15 +1,16 @@
 const net = require("net");
+const { IP, PORT, NAME } = require('./constant');
 const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost',// IP address here,
-    port: 50541,// PORT number here,
+    host: IP,// IP address here,
+    port: PORT,// PORT number here,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on('connect', () => {
     console.log(`Successfully connected to game server`);
-    conn.write('Name: HA7');
+    conn.write(`Name: ${NAME}`);
     // setInterval(() => {
     //   conn.write('Move: up')
     // }, 1000);
